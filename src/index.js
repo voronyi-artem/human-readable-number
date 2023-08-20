223321dd9a03741c7a08422a89d99187db5fc553
Artem Voronyi <voronyi.artem@gmail.com>
2023-08-20 16:41:57 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const toTen = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const toTwenty = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tense = [
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let stringNumber = number.toString();
+    if (stringNumber.length == 1) {
+        return stringNumber[0] == "0" ? "zero" : toTen[number];
+    } else if (stringNumber.length == 2) {
+        return (
+            (stringNumber[0] != "0" && stringNumber[0] != "1" ? "" + tense[+stringNumber[0] - 1] : "") +
+            (stringNumber[0] == "1" ? toTwenty[stringNumber[1]] : "") +
+            (stringNumber[1] != "0" && stringNumber[0] != "1" ? " " + toTen[stringNumber[1]] : "")
+        );
+    } else if (stringNumber.length == 3) {
+        return (
+            toTen[stringNumber[0]] +
+            " " +
+            "hundred" +
+            (stringNumber[1] != "0" && stringNumber[1] != "1" ? " " + tense[+stringNumber[1] - 1] : "") +
+            (stringNumber[1] == "1" ? " " + toTwenty[stringNumber[2]] : "") +
+            (stringNumber[2] != "0" && stringNumber[1] != "1" ? " " + toTen[stringNumber[2]] : "")
+        );
+    }
+};
